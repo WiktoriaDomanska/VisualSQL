934b5020e6d5317df63755bc2e83465a305ea32d
WiktoriaDomanska <wiktoria170504@gmail.com>
2026-03-17 19:44:51 +0100

    Integracja Node Editor i stworzenie sztywnego szkieletu UI

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include "Table.hpp"
 #include "Column.hpp"
+#include <imgui_node_editor.h>
+namespace ed = ax::NodeEditor;
 
 int main() {
     if (!glfwInit()) return -1;
@@ -34,6 +36,11 @@ int main() {
     usersTable.addColumn(Column("email", "VARCHAR(255)"));
     usersTable.addColumn(Column("created_at", "TIMESTAMP"));
 
+    ed::Config config;
+    config.SettingsFile = "VisualSQL_NodeEditor.json";
+    ed::EditorContext* m_Context = ed::CreateEditor(&config);
+    ed::SetCurrentEditor(m_Context);
+
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
 
@@ -41,28 +48,73 @@ int main() {
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        // Nasze pierwsze okienko
-        ImGui::Begin("VisualSQL Panel Sterowania");
-        ImGui::Text("Witaj w VisualSQL!");
-
-        ImGui::Separator();
-
-        ImGui::Text("Tabela: %s", usersTable.getName().c_str());
-
-        for (size_t i = 0; i < usersTable.getColumns().size(); i++) {
-            Column col = usersTable.getColumns()[i];
-
-            ImGui::BulletText("%s : %s", col.getName().c_str(), col.getType().c_str());
+        if (ImGui::BeginMainMenuBar()) {
+            if (ImGui::BeginMenu("File")) {
+                ImGui::MenuItem("Zapisz", "Ctrl+S");
+                ImGui::MenuItem("Zakoncz");
+                ImGui::EndMenu();
+            }
+            if (ImGui::BeginMenu("Edit")) {
+                ImGui::MenuItem("Cofnij");
+                ImGui::EndMenu();
+            }
+            if (ImGui::BeginMenu("View")) {
+                ImGui::MenuItem("Prybliz");
+                ImGui::EndMenu();
+            }
+            ImGui::EndMainMenuBar();
         }
 
-        ImGui::Separator();
+        const ImGuiViewport* viewport = ImGui::GetMainViewport();
+        float screenWidth = viewport->WorkSize.x;
+        float screenHeight = viewport->WorkSize.y;
 
-        if (ImGui::Button("Kliknij mnie!")) {
-            std::cout << "Przycisk dziala!" << std::endl;
+        float leftPanelWidth = 60.0f;
+        float rightPanelWidth = 250.0f;
+
+        ImGuiWindowFlags panelFlags = ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoTitleBar;
+
+        ImGui::SetNextWindowPos(ImVec2(viewport->WorkPos.x, viewport->WorkPos.y));
+        ImGui::SetNextWindowSize(ImVec2(leftPanelWidth, screenHeight));
+
+        ImGui::Begin("LeftPanel", nullptr, panelFlags);
+        ImGui::Text("Narzedzia");
+        ImGui::Separator();
+        if (ImGui::Button("+", ImVec2(40, 40))) {
+            // W przyszłości będzie tu dodana nowa tabela
         }
         ImGui::End();
 
+        ImGui::SetNextWindowPos(ImVec2(viewport->WorkPos.x + screenWidth - rightPanelWidth, viewport->WorkPos.y));
+        ImGui::SetNextWindowSize(ImVec2(rightPanelWidth, screenHeight));
+
+        ImGui::Begin("RightPanel", nullptr, panelFlags);
+        ImGui::Text("Wlasciwosci");
+        ImGui::Separator();
+        ImGui::Text("Wybierz tabele..."); // Tu pojawią się detale klikniętej tabeli
+        ImGui::End();
+
+        ImGui::SetNextWindowPos(ImVec2(viewport->WorkPos.x + leftPanelWidth, viewport->WorkPos.y));
+        ImGui::SetNextWindowSize(ImVec2(screenWidth - leftPanelWidth - rightPanelWidth, screenHeight));
+
+        ImGui::Begin("Workspace", nullptr, panelFlags);
+
+        ed::Begin("My Editor", ImVec2(0.0, 0.0f));
+
+        ed::BeginNode(1);
+        ImGui::Text("Tabela: %s", usersTable.getName().c_str());
+        ImGui::Separator();
+        for (size_t i = 0; i < usersTable.getColumns().size(); i++) {
+            Column col = usersTable.getColumns()[i];
+            ImGui::Text("%s : %s", col.getName().c_str(), col.getType().c_str());
+        }
+        ed::EndNode();
+
+        ed::End();
+        ImGui::End();
+
         ImGui::Render();
+
         int display_w, display_h;
         glfwGetFramebufferSize(window, &display_w, &display_h);
         glViewport(0, 0, display_w, display_h);
@@ -76,6 +128,7 @@ int main() {
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
     ImGui::DestroyContext();
+    ed::DestroyEditor(m_Context);
     glfwDestroyWindow(window);
     glfwTerminate();
 
